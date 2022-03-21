38f4ffa58512b7f1ad84b7b4a93eb51d6191d45b
Sonnik9 <nikolas.smsttt@gmail.com>
2022-03-21 21:44:19 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,397 @@
+
+let dataItems = [
+    {
+        "age": 0,
+        "id": "motorola-xoom-with-wi-fi",
+        "imageUrl": "img/phones/motorola-xoom-with-wi-fi.0.jpg",
+        "name": "Motorola XOOM\u2122 with Wi-Fi",
+        "snippet": "The Next, Next Generation\r\n\r\nExperience the future with Motorola XOOM with Wi-Fi, the world's first tablet powered by Android 3.0 (Honeycomb)."
+      },
+      {
+        "age": 1,
+        "id": "motorola-xoom",
+        "imageUrl": "img/phones/motorola-xoom.0.jpg",
+        "name": "MOTOROLA XOOM\u2122",
+        "snippet": "The Next, Next Generation\n\nExperience the future with MOTOROLA XOOM, the world's first tablet powered by Android 3.0 (Honeycomb)."
+      },
+      {
+        "age": 2,
+        "carrier": "AT&amp;T",
+        "id": "motorola-atrix-4g",
+        "imageUrl": "img/phones/motorola-atrix-4g.0.jpg",
+        "name": "MOTOROLA ATRIX\u2122 4G",
+        "snippet": "MOTOROLA ATRIX 4G the world's most powerful smartphone."
+      },
+      {
+        "age": 3,
+        "id": "dell-streak-7",
+        "imageUrl": "img/phones/dell-streak-7.0.jpg",
+        "name": "Dell Streak 7",
+        "snippet": "Introducing Dell\u2122 Streak 7. Share photos, videos and movies together. It\u2019s small enough to carry around, big enough to gather around."
+      },
+      {
+        "age": 4,
+        "carrier": "Cellular South",
+        "id": "samsung-gem",
+        "imageUrl": "img/phones/samsung-gem.0.jpg",
+        "name": "Samsung Gem\u2122",
+        "snippet": "The Samsung Gem\u2122 brings you everything that you would expect and more from a touch display smart phone \u2013 more apps, more features and a more affordable price."
+      },
+      {
+        "age": 5,
+        "carrier": "Dell",
+        "id": "dell-venue",
+        "imageUrl": "img/phones/dell-venue.0.jpg",
+        "name": "Dell Venue",
+        "snippet": "The Dell Venue; Your Personal Express Lane to Everything"
+      },
+      {
+        "age": 6,
+        "carrier": "Best Buy",
+        "id": "nexus-s",
+        "imageUrl": "img/phones/nexus-s.0.jpg",
+        "name": "Nexus S",
+        "snippet": "Fast just got faster with Nexus S. A pure Google experience, Nexus S is the first phone to run Gingerbread (Android 2.3), the fastest version of Android yet."
+      },
+      {
+        "age": 7,
+        "carrier": "Cellular South",
+        "id": "lg-axis",
+        "imageUrl": "img/phones/lg-axis.0.jpg",
+        "name": "LG Axis",
+        "snippet": "Android Powered, Google Maps Navigation, 5 Customizable Home Screens"
+      },
+      {
+        "age": 8,
+        "id": "samsung-galaxy-tab",
+        "imageUrl": "img/phones/samsung-galaxy-tab.0.jpg",
+        "name": "Samsung Galaxy Tab\u2122",
+        "snippet": "Feel Free to Tab\u2122. The Samsung Galaxy Tab\u2122 brings you an ultra-mobile entertainment experience through its 7\u201d display, high-power processor and Adobe\u00ae Flash\u00ae Player compatibility."
+      },
+      {
+        "age": 9,
+        "carrier": "Cellular South",
+        "id": "samsung-showcase-a-galaxy-s-phone",
+        "imageUrl": "img/phones/samsung-showcase-a-galaxy-s-phone.0.jpg",
+        "name": "Samsung Showcase\u2122 a Galaxy S\u2122 phone",
+        "snippet": "The Samsung Showcase\u2122 delivers a cinema quality experience like you\u2019ve never seen before. Its innovative 4\u201d touch display technology provides rich picture brilliance, even outdoors"
+      },
+      {
+        "age": 10,
+        "carrier": "Verizon",
+        "id": "droid-2-global-by-motorola",
+        "imageUrl": "img/phones/droid-2-global-by-motorola.0.jpg",
+        "name": "DROID\u2122 2 Global by Motorola",
+        "snippet": "The first smartphone with a 1.2 GHz processor and global capabilities."
+      },
+      {
+        "age": 11,
+        "carrier": "Verizon",
+        "id": "droid-pro-by-motorola",
+        "imageUrl": "img/phones/droid-pro-by-motorola.0.jpg",
+        "name": "DROID\u2122 Pro by Motorola",
+        "snippet": "The next generation of DOES."
+      },
+      {
+        "age": 12,
+        "carrier": "AT&amp;T",
+        "id": "motorola-bravo-with-motoblur",
+        "imageUrl": "img/phones/motorola-bravo-with-motoblur.0.jpg",
+        "name": "MOTOROLA BRAVO\u2122 with MOTOBLUR\u2122",
+        "snippet": "An experience to cheer about."
+      },
+      {
+        "age": 13,
+        "carrier": "T-Mobile",
+        "id": "motorola-defy-with-motoblur",
+        "imageUrl": "img/phones/motorola-defy-with-motoblur.0.jpg",
+        "name": "Motorola DEFY\u2122 with MOTOBLUR\u2122",
+        "snippet": "Are you ready for everything life throws your way?"
+      },
+      {
+        "age": 14,
+        "carrier": "T-Mobile",
+        "id": "t-mobile-mytouch-4g",
+        "imageUrl": "img/phones/t-mobile-mytouch-4g.0.jpg",
+        "name": "T-Mobile myTouch 4G",
+        "snippet": "The T-Mobile myTouch 4G is a premium smartphone designed to deliver blazing fast 4G speeds so that you can video chat from practically anywhere, with or without Wi-Fi."
+      },
+      {
+        "age": 15,
+        "carrier": "US Cellular",
+        "id": "samsung-mesmerize-a-galaxy-s-phone",
+        "imageUrl": "img/phones/samsung-mesmerize-a-galaxy-s-phone.0.jpg",
+        "name": "Samsung Mesmerize\u2122 a Galaxy S\u2122 phone",
+        "snippet": "The Samsung Mesmerize\u2122 delivers a cinema quality experience like you\u2019ve never seen before. Its innovative 4\u201d touch display technology provides rich picture brilliance,even outdoors"
+      },
+      {
+        "age": 16,
+        "carrier": "Sprint",
+        "id": "sanyo-zio",
+        "imageUrl": "img/phones/sanyo-zio.0.jpg",
+        "name": "SANYO ZIO",
+        "snippet": "The Sanyo Zio by Kyocera is an Android smartphone with a combination of ultra-sleek styling, strong performance and unprecedented value."
+      },
+      {
+        "age": 17,
+        "id": "samsung-transform",
+        "imageUrl": "img/phones/samsung-transform.0.jpg",
+        "name": "Samsung Transform\u2122",
+        "snippet": "The Samsung Transform\u2122 brings you a fun way to customize your Android powered touch screen phone to just the way you like it through your favorite themed \u201cSprint ID Service Pack\u201d."
+      },
+      {
+        "age": 18,
+        "id": "t-mobile-g2",
+        "imageUrl": "img/phones/t-mobile-g2.0.jpg",
+        "name": "T-Mobile G2",
+        "snippet": "The T-Mobile G2 with Google is the first smartphone built for 4G speeds on T-Mobile's new network. Get the information you need, faster than you ever thought possible."
+      },
+      {
+        "age": 19,
+        "id": "motorola-charm-with-motoblur",
+        "imageUrl": "img/phones/motorola-charm-with-motoblur.0.jpg",
+        "name": "Motorola CHARM\u2122 with MOTOBLUR\u2122",
+        "snippet": "Motorola CHARM fits easily in your pocket or palm.  Includes MOTOBLUR service."
+      }
+
+
+];
+
+
+let inputEl = document.querySelector('#my-input');
+let selectEl = document.querySelector('#my-select');
+let maneger = document.querySelector('.maneger');
+let products = document.querySelector('.products');
+let ulList = document.querySelector('.ul-list');
+let wrapper = document.querySelector('.wrapper');
+
+let tit1;
+let tit2;
+let tit3;
+let tit4;
+let tit21;
+let tit20;
+let cont1;
+let cont2;
+let img1;
+let img21;
+let img2;
+let img20;
+let descript1;
+let descript2;
+let descript20;
+let descript21;
+let li1;
+let i;
+let val;
+let elasticItems;
+let curentValHref;
+
+let pi;
+let list;
+let value;
+
+inputEl.addEventListener('input', searchingItems);
+selectEl.addEventListener('change', sortBy);
+           
+     function generationList() {
+     
+       for (i = 0; i < dataItems.length; i++) {
+       
+         li1 = document.createElement('li');
+         li1.classList.add('item');
+         ulList.append(li1);
+
+         curentValHref = '#' + dataItems[i].id;
+     
+         tit1 = document.createElement('a');
+         tit1.classList.add('title-item');
+         tit1.setAttribute('href', curentValHref);
+      
+         li1.append(tit1);
+         tit2 = document.querySelectorAll('.title-item');
+         tit2[i].textContent = dataItems[i].name;
+     
+         img1 = document.createElement('img');
+         img1.classList.add('img-item');
+         li1.append(img1);
+     
+         img2 = document.querySelectorAll('.img-item');
+         img2[i].src = dataItems[i].imageUrl;
+     
+         descript1 = document.createElement('p');
+         descript1.classList.add('descript-item');
+         li1.append(descript1);
+         descript2 = document.querySelectorAll('.descript-item');
+         descript2[i].textContent = dataItems[i].snippet;
+       }
+
+       location.hash = 'list';
+         
+     }
+     
+     generationList();
+
+     function removeList() {
+       
+        document.querySelectorAll('li').forEach(li => li.remove());
+     }
+
+
+    function sortBy() {
+       
+        if(selectEl.value == 'random') {
+            removeList()
+            for (var i = 0; i < 5; i++){
+              dataItems.sort(()=> Math.random() - 0.5);
+            }
+              generationList();
+              inputEl.value = '';
+         
+        }
+
+        if(selectEl.value == 'newest') {
+            removeList()
+            dataItems.sort((a, b)=> a.age > b.age ? 1: -1);
+            generationList();
+            inputEl.value = ''; 
+              
+        }
+       
+        if(selectEl.value == 'alphabet') {
+            removeList()
+            dataItems.sort((a, b)=> a.id > b.id ? 1: -1);
+            generationList();
+            inputEl.value = '';
+         
+        }
+      
+    }
+
+    function searchingItems() {
+         value = inputEl.value.toLowerCase().trim();
+      
+         list = document.querySelectorAll('.item');
+      
+	       if (value !='') {
+	       	list.forEach(elem => {
+	       		if (elem.firstChild.innerText.toLowerCase().search(value) == -1) {
+             
+	       			elem.classList.add('hide');
+              elem.firstChild.innerHTML = elem.firstChild.innerText;
+              // console.log(elem)
+	       		}
+
+             else {
+             
+               elem.classList.remove('hide');
+               let str = elem.firstChild.innerText;
+              //  console.log(elem)
+               elem.firstChild.innerHTML = insertMark(str,elem.firstChild.innerText.toLowerCase().search(value),value.length);
+             }
+	       	});
+	       } 
+            else {
+            
+	       	   list.forEach(elem => {
+	       		 elem.classList.remove('hide');
+             console.log(elem)
+             elem.firstChild.innerHTML = elem.firstChild.innerText;
+            
+	       	})
+	       }
+         
+    }    
+
+    function insertMark(string,pos,len) {
+      return string.slice(0, pos) + '<mark>' + string.slice(pos, pos+len) + '</mark>' + string.slice(pos+len);
+    }
+         
+
+    // cart items start 
+    window.addEventListener('hashchange', generationCartItems);
+  
+    function delitePageOne() {
+      maneger.style.display = 'none';
+      products.style.display = 'none';
+
+    }
+
+    function recreatePageOne() {
+      maneger.style.display = 'block';
+      products.style.display = 'block';
+      
+    }
+   
+    function createPageTwo() {
+         cont1 = document.createElement('div');
+         cont1.classList.add('cont');
+         wrapper.append(cont1);
+         cont2 = document.querySelector('.cont');
+
+         let leftSide = document.createElement('div');
+         leftSide.classList.add('leftSide');
+         cont2.append(leftSide);
+       
+         img21 = document.createElement('img');
+         img21.classList.add('item2');
+         leftSide.append(img21);
+         img20 = document.querySelector('.item2');
+
+         let rightSide = document.createElement('div');
+         rightSide.classList.add('right-side');
+         cont2.append(rightSide);
+
+         tit21 = document.createElement('p');
+         tit21.classList.add('title-item2');
+        
+         rightSide.append(tit21);
+         tit20 = document.querySelector('.title-item2');
+        
+
+         descript21 = document.createElement('p');
+         descript21.classList.add('descript20-item');
+         rightSide.append(descript21);
+        
+         descript20 = document.querySelector('.descript20-item');
+
+         let hr = document.createElement('hr');
+         hr.classList.add('hr');
+         rightSide.append(hr);
+       
+
+         for (let h = 0; h <  dataItems.length; h++) {
+          if('#' + dataItems[h].id == location.hash)
+          img20.src = dataItems[h].imageUrl;
+          console.log('hhgghgggfg');
+          tit20.textContent = dataItems[h].name;
+          descript20.textContent = dataItems[h].snippet;
+           
+         }
+     
+    }
+
+    function delitePageTwo() {
+      for (let h = 0; h <  dataItems.length; h++) {
+        if(location.hash == '#list')
+        cont2.remove();
+
+      }
+    }
+
+    function generationCartItems() {
+     
+     
+      if(location.hash !== '#list') 
+      { delitePageOne();
+       createPageTwo();}
+
+      else
+     
+      {recreatePageOne();
+      delitePageTwo();}
+      }
+ 
+          
+
+          
